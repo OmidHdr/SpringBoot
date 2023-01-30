ce7660b2888d72b09c2ecda84a1d85987f9bb946
OmidHdr <omidnet42@yahoo.com>
2023-01-30 20:04:35 +0330

    Add Entity

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -3,9 +3,12 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Application {
 
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
